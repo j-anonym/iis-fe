cd96b7d1e743e7d8e041ea8a379f8969a2f246d9
alesed <xsedla1c@vutbr.cz>
2019-11-25 20:43:47 +0100

    create tournament form

--- src/app/tournaments-create/tournaments-create.component.ts
+++ src/app/tournaments-create/tournaments-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { TournamentsCreateService } from './tournaments-create.service';
 
 @Component({
@@ -9,25 +9,29 @@ import { TournamentsCreateService } from './tournaments-create.service';
 })
 export class TournamentsCreateComponent implements OnInit {
 
-  createForm;
+  public createForm: FormGroup;
   singles = true;
   gender = '---';
 
-  constructor(private formBuilder : FormBuilder, private createService : TournamentsCreateService) { 
-    this.createForm = this.formBuilder.group({
-      name: null,
-      date: null,
-      place: null,
-      is_singles: null,
-      type: null,
-      capacity: null,
-      prize: null,
-      cost: null,
-      sponsors: null
+  constructor(private createService : TournamentsCreateService) { }
+
+  ngOnInit() {
+    this.createForm = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.maxLength(128)]),
+      date_from: new FormControl(null, [Validators.required]),
+      date_to: new FormControl(null, [Validators.required]),
+      place: new FormControl(null, [Validators.required]),
+      is_singles: new FormControl(null, [Validators.required]),
+      type: new FormControl(null, [Validators.required]),
+      capacity: new FormControl(null, [Validators.required, Validators.max(64)]),
+      prize: new FormControl(null, [Validators.required]),
+      cost: new FormControl(null, [Validators.required]),
+      sponsors: new FormControl('', [Validators.maxLength(512)])
     });
   }
 
-  ngOnInit() {
+  public hasError = (controlName: string, errorName: string) =>{
+    return this.createForm.controls[controlName].hasError(errorName);
   }
 
   submit(data) {
