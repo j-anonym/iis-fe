dc6420e3f6f1d88b55f3a447dac7bbc1c558a56f
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-28 18:36:18 +0100

    update, router na dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
   checkLogin() {
     (this.loginservice.authenticate(this.username, this.password).subscribe(
             data => {
-              this.router.navigate([''])
+              this.router.navigate(['/dashboard'])
               this.invalidLogin = false
             },
             error => {
