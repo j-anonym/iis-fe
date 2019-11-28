0aa5f79d9778561010a80e4c626c51109250c7b7
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-28 22:43:16 +0100

    spravila som skaredu vec
    nastavila som si routingy na admin a statistics lebo neviem nastavit router na tu ikonku:D

---
+++ src/app/login/authentication.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+ export class Account{
+    constructor(
+        public status:string,
+    ) {}
+
+}
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+
+    constructor(
+        private httpClient:HttpClient
+    ) {
+    }
+
+    authenticate(username, password) {
+        return this.httpClient.post<any>('http://localhost:8087/authenticate',{username,password}).pipe(
+            map(
+                userData => {
+                    sessionStorage.setItem('username',username);
+                    let tokenStr= 'Bearer '+userData.token;
+                    sessionStorage.setItem('token', tokenStr);
+                    return userData;
+                }
+            )
+
+        );
+    }
+
+    register(username, password, name, surname, nationality) {
+        return this.httpClient.post<any>('http://localhost:8087/register',{username,password, name, surname, nationality}).pipe(
+            map(
+                userData => {
+                    sessionStorage.setItem('username',username);
+                    return userData;
+                }
+            )
+
+        );
+    }
+
+
+
+    isUserLoggedIn() {
+        let account = sessionStorage.getItem('username')
+        console.log(!(account === null))
+        return !(account === null)
+    }
+
+    logOut() {
+        sessionStorage.removeItem('username')
+    }
+}
