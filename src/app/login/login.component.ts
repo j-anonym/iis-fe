147675a17e5dfd16744c66f67b4c8bd8278fdd2a
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-28 17:46:01 +0100

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = ''
+  password = ''
+  invalidLogin = false
+
+  constructor(private router: Router,
+              private loginservice: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  checkLogin() {
+    (this.loginservice.authenticate(this.username, this.password).subscribe(
+            data => {
+              this.router.navigate([''])
+              this.invalidLogin = false
+            },
+            error => {
+              this.invalidLogin = true
+
+            }
+        )
+    );
+
   }
 
 }
