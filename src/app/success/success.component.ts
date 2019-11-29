05e40a263745e3fc2fa1a82990f0bce337238298
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-29 17:30:14 +0100

    success

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,21 +1,17 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { Account} from "../login/account";
 import { AuthenticationService} from "../login/authentication.service";
+import { AccountService} from "../login/account.service";
 import {first} from "rxjs/operators";
 
 @Component({ templateUrl: 'success.component.html' })
 export class SuccessComponent {
-    loading = false;
-    private curr_account = '';
-
-    constructor(private authService: AuthenticationService){ this.curr_account = authService.currentVal.username}
+    current_acc = '';
+    constructor(
+        private accountService: AccountService,
+        private authService: AuthenticationService){this.current_acc = this.authService.currentVal.token}
 
     ngOnInit() {
-        this.loading = true;
-         //this.curr_account = this.authService.currentVal.username;
-        //this.curr_account = localStorage.getItem('username');
-      //      this.loading = false;
-        //    this.
-        //})
+        console.log(this.current_acc);
     }
 }
