221e34c35f1ad978789b6c727181ccf3ff23c75c
Jan Vavro <vavrojj@gmail.com>
2019-12-01 20:19:34 +0100

    fix validator

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,9 +5,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
@@ -16,29 +16,28 @@ export class RegisterComponent implements OnInit {
     submitted = false;
     error = '';
 
-  constructor(private router: Router,
-              private formBuilder: FormBuilder,
-              private route: ActivatedRoute,
-              private registerservice: AuthenticationService)
-  {}
+    constructor(private router: Router,
+                private formBuilder: FormBuilder,
+                private route: ActivatedRoute,
+                private registerservice: AuthenticationService) { }
 
 
-  ngOnInit() {
-      this.loginForm = this.formBuilder.group({
-          username: ['', Validators.required],
-          password: ['', Validators.required],
-          name: ['', Validators.required],
-          surname: ['', Validators.required],
-          nationality:['', Validators.required, Validators.maxLength(2), Validators.minLength(2)],
-      });
-  }
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            nationality: ['', [Validators.required, Validators.maxLength(2), Validators.minLength(2)]],
+        });
+    }
 
-  get f() {return this.loginForm.controls;}
+    get f() { return this.loginForm.controls; }
 
 
-  public hasError = (controlName: string, errorName: string) => {
-    return this.loginForm.controls[controlName].hasError(errorName);
-  }
+    public hasError = (controlName: string, errorName: string) => {
+        return this.loginForm.controls[controlName].hasError(errorName);
+    }
 
     onSubmit() {
         this.submitted = true;
@@ -49,14 +48,15 @@ export class RegisterComponent implements OnInit {
 
         this.loading = true;
 
-        this.registerservice.register(this.f.username.value, this.f.password.value, this.f.name.value, this.f.surname.value, this.f.nationality.value).pipe(first()).subscribe(
-            data => {
-                this.router.navigate(['/login']);
-            },
-            error => {
-                this.error = 'Username is already taken'
-                this.loading = false;
-            });
+        this.registerservice.register(this.f.username.value, this.f.password.value, this.f.name.value, this.f.surname.value,
+            this.f.nationality.value).pipe(first()).subscribe(
+                data => {
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.error = 'Username is already taken';
+                    this.loading = false;
+                });
     }
 
 }
