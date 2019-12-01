78607a920422c75d52e4f5dac9c6997b033ccfa7
Jan Vavro <vavrojj@gmail.com>
2019-12-01 20:55:03 +0100

    some fixes

--- src/app/login/account.service.ts
+++ src/app/login/account.service.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class AccountService {
-    constructor(private http:HttpClient) {}
+    constructor(private http: HttpClient) {}
 
     getLoggedUserId(username) {
-        return this.http.get("/api/person/getloggeduserid/" + username);
+        return this.http.get('/api/person/getloggeduserid/' + username);
     }
 
     getLoggedUserAdminStatus(username) {
-        return this.http.get("/api/person/getloggeduseradminstatus/" + username);
+        return this.http.get('/api/person/getloggeduseradminstatus/' + username);
     }
 }
