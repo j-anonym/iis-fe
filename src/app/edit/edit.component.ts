8f7105731f75c764366fe822eb1defd0caf7b935
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-12-02 11:06:41 +0100

    update

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
-import * as moment from "moment";
 import {Globals} from "../globals";
+import * as jwt_decode from 'jwt-decode';
 import { AdminPageService } from '../admin-page/admin-page.service';
+import {AuthenticationService} from "../login/authentication.service";
+import {AccountService} from "../login/account.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {first} from "rxjs/operators";
 
 @Component({
     selector: 'app-edit',
@@ -11,43 +15,120 @@ import { AdminPageService } from '../admin-page/admin-page.service';
 })
 
 export class EditComponent implements OnInit {
-
-    public createForm: FormGroup;
-    currentDate: Date;
+    editForm: FormGroup;
+    loading = false;
+    submitted = false;
+    error = '';
+    tok = '';
+    usrnm = '';
+    name = '';
     loggedUser;
 
+    constructor(private router: Router,
+                private formBuilder: FormBuilder,
+                private route: ActivatedRoute,
+                private globals: Globals,
+                private userService: AdminPageService,
+                private accService: AccountService,
+                private authService: AuthenticationService) {this.tok = this.authService.currentVal.token;}
 
-    constructor(
-        private globals: Globals,
-        private userService:AdminPageService)
-    {}
 
     ngOnInit() {
+        const decoded = jwt_decode(this.tok);
+        this.usrnm = decoded.sub;
+        this.globals.loggeduser = this.usrnm;
+        this.accService.getLoggedUserId(this.usrnm).subscribe(result => {
+            this.globals.loggeduserid = JSON.parse(JSON.stringify(result));
+            console.log(this.globals.loggeduserid);
+        });
         this.userService.getUser(this.globals.loggeduserid).subscribe( user => {
             this.loggedUser = JSON.parse(JSON.stringify(user));
-            console.log(this.loggedUser);
+            this.name = this.loggedUser['name'];
+            console.log(this.name);
+        })
+        console.log(this.name);
+        this.editForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            nationality:['', Validators.required],
+            birth:[''],
+            sex:[''],
+            left_handed:['']
         });
-
-        this.currentDate = moment().toDate();
-        const date = new Date(this.loggedUser.birth);
-
-        this.createForm = new FormGroup({
-            name: new FormControl(this.loggedUser.name, [ Validators.maxLength(128), Validators.required ]),
-            surname: new FormControl(this.loggedUser.surname, [ Validators.maxLength(128), Validators.required ]),
-            birth: new FormControl(date),
-            nationality: new FormControl(this.loggedUser.nationality, [ Validators.maxLength(2), Validators.minLength(2), Validators.required ]),
-            sex: new FormControl(this.loggedUser.sex),
-            is_left_handed: new FormControl(this.loggedUser.is_left_handed),
-        });
-
     }
 
-    public hasError = (controlName: string, errorName: string) => {
-        return this.createForm.controls[controlName].hasError(errorName);
-    }
+    get f() {return this.editForm.controls;}
 
-    submit(data) {
-        this.userService.updateUser(data);
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.editForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+
+        if(this.f.birth.value) {
+            this.userService.updateBirth(this.globals.loggeduserid, this.f.birth.value).subscribe(
+                data => {
+                },
+                error => {
+                    this.error = 'Date is not valid'
+                    this.loading = false;
+                });
+        }
+
+        if(this.f.left_handed.value) {
+            if(this.f.left_handed.value === 'Yes') {
+                this.userService.updateLeftHanded(this.globals.loggeduserid, true).subscribe(
+                    data => {
+                    },
+                    error => {
+                    });
+            }
+            else {
+                this.userService.updateLeftHanded(this.globals.loggeduserid, false).subscribe(
+                    data => {
+                    },
+                    error => {
+                    });
+            }
+        }
+
+        if(this.f.sex.value) {
+            if(this.f.sex.value === 'M') {
+                this.userService.updateLeftHanded(this.globals.loggeduserid, 'M').subscribe(
+                    data => {
+                    },
+                    error => {
+                    });
+            }
+            else {
+                this.userService.updateLeftHanded(this.globals.loggeduserid, 'W').subscribe(
+                    data => {
+                    },
+                    error => {
+                    });
+            }
+        }
+
+        this.userService.updateName(this.globals.loggeduserid, this.f.name.value).subscribe(
+            data => {
+            },
+            error => {
+            });
+        this.userService.updateSurname(this.globals.loggeduserid, this.f.surname.value).subscribe(
+            data => {
+            },
+            error => {
+            });
+        this.userService.updateNationality(this.globals.loggeduserid, this.f.nationality.value).subscribe(
+            data => {
+                this.router.navigate(['/success/edit']);
+            },
+            error => {
+            });
+
     }
 
 }
