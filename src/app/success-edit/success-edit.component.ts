70162bfd1046a54a466445ea247524392be56019
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-12-02 11:08:46 +0100

    success edit

---
+++ src/app/success-edit/success-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-edit',
+  templateUrl: './success-edit.component.html',
+  styleUrls: ['./success-edit.component.scss']
+})
+export class SuccessEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
